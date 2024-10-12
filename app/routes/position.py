36a075ff4adda9860137ed04a19638507f063cd0
Dartty123 <dariyatamanuk@icloud.com>
2024-10-12 10:00:44 +0300

    аа

--- app/routes/position.py
+++ app/routes/position.py
@@ -1,50 +1,27 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, redirect
 
-from app.db import Session, User, Post
+from app.db import Session, Position
 from app.data.password import ADMIN_PASS
 
+position_route = Blueprint("positions", __name__, url_prefix="/positions/")
 
-post_route = Blueprint("posts", __name__)
 
-
-@post_route.get("/posts/")
-@post_route.post("/posts/")
-def add_post():
+@position_route.get("/")
+@position_route.post("/")
+def add_position():
     msg = ""
     block = False
-
     with Session() as session:
         if request.method == "POST":
-            title = request.form.get("title")
-            text = request.form.get("text")
-            user_id = request.form.get("user_id")
+            name = request.form.get("name")
+            password = request.form.get("password")
 
-            if request.form.get("password") == ADMIN_PASS:
-                post = Post(title=title, text=text, user_id=user_id)
-                session.add(post)
+            if password == ADMIN_PASS:
+                position = Position(name=name)
+                session.add(position)
                 session.commit()
-                msg = "Статтю успішно додано"
+                msg = "Нова посада успішно додана"
             else:
                 block = True
 
-        users = session.query(User).all()
-        context = {
-            "msg": msg,
-            "block": block,
-            "users": users
-        }
-        return render_template("add_post.html", **context)
-
-
-@post_route.get("/")
-def index():
-    with Session() as session:
-        posts = session.query(Post).all()
-        return render_template("index.html", posts=posts)
-
-
-@post_route.get("/post/<int:id>")
-def get_post(id):
-    with Session() as session:
-        post = session.query(Post).where(Post.id == id).first()
-        return render_template("post.html", post=post)
+    return render_template("add_position.html", msg=msg, block=block)
